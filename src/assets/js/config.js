62588e2bb05c01ab5a2a70a4e3ad660e05a13113
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-08-23 23:28:50 +0100

    Updates API url

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:5000/';
+export const API_URL = 'https://shur.netlify.app/url/';
 export const PERSIST_URL_API_ENDPOINT = API_URL + 'shorten';
 export const TIMEOUT_SEC = 10;
